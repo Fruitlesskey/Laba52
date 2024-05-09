17e0ebce31b43aec0cecb0f02f5f9757d77c3dd5
Albert Islamau <fruitlesskey2@mail.ru>
2024-05-09 22:50:19 +0300

    add csv tests

--- src/test/java/com/example/laba52/BonusServiceTest.java
+++ src/test/java/com/example/laba52/BonusServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BonusServiceTest {
 
@@ -29,5 +31,19 @@ public class BonusServiceTest {
     int expected = 2;
     int actual = service.calculateMonthsOfRest(income, expenses, threshold);
     Assertions.assertEquals(expected, actual);
+
+
+
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "10000, 3000, 20000, 3",
+      "100000, 60000, 150000, 2"
+  })
+  void shouldCalculateRestMonthsBasedOnCsvSource(int income, int expenses, int threshold, int expected) {
+    BonusService service = new BonusService();
+    int actual = service.calculateMonthsOfRest(income, expenses, threshold);
+    assertEquals(expected, actual);
   }
 }
